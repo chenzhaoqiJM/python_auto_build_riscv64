1e340f375410deb99ee2d9f3b7020040cb5fd237
chenzhaoqi <zhaoqi.chen@spacemit.com>
2026-04-10 13:12:35 +0800

    Fix ABI

--- common_py/00upload_with_repair.py
+++ common_py/00upload_with_repair.py
@@ -143,6 +143,9 @@ def upload_whl(whl_path: Path):
     if 'none-any' in str(whl_path.name):
         print("none-any 包，不上传")
         return
+    if 'abi3' in whl_path.name and sys.version_info > (3, 12):
+        print(f"检测到 abi3 包且当前 Python 版本为 {sys.version_info.major}.{sys.version_info.minor}，跳过上传: {whl_path.name}")
+        return
     print(f"🚀 Uploading {whl_path.name} to PyPI repo: {PYPI_REPO}")
     subprocess.run([
         "twine", "upload", "-r", PYPI_REPO, str(whl_path)
